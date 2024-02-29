fbeb94b185ae11fe88c68f405832bf17615c4318
FacuGuillen <facu.guillen@hotmail.com>
2024-02-29 20:54:45 -0300

    Agregue popup sección registrarse

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@ const openLogin = document.getElementById('open-login')
 const user = document.getElementById('user')
 const openImg = document.getElementById('login-img')
 
-document.addEventListener ('click', function(e){
-    if(!openLogin.contains(e.target) && !openImg.contains(e.target)){
+document.addEventListener('click', function(e) {
+    if ((!openLogin.contains(e.target) && !openImg.contains(e.target)) || user.contains(e.target)) {
         user.classList.add('d-none');
     }
-})
+});
 
-openLogin.addEventListener ("click" , function(e){
+openLogin.addEventListener("click", function(e) {
     e.preventDefault();
     user.classList.toggle("d-none");
-})
+});
 
-openImg.addEventListener ('click', function(e){
+openImg.addEventListener('click', function(e) {
     e.preventDefault();
     user.classList.toggle('d-none');
-})
+});
+
+user.addEventListener('click', function(e) {
+    e.stopPropagation();
+});
